cc18ece8a596bca54a52799d26f41d9137075de2
rianwnoviantoro <tenkeylost@gmail.com>
2023-03-13 14:13:06 +0700

    refactoring

--- src/main/kotlin/com/rianwnoviantoro/jwt/interceptors/HeadersInterceptor.kt
+++ src/main/kotlin/com/rianwnoviantoro/jwt/interceptors/HeadersInterceptor.kt
@@ -8,13 +8,29 @@ import org.springframework.web.context.request.WebRequestInterceptor
 import java.lang.Exception
 
 @Component
-class TransportSecurityInterceptor(): WebRequestInterceptor {
+class HeadersInterceptor(): WebRequestInterceptor {
     override fun preHandle(request: WebRequest) {
+        val contentTypeOption = request.getHeader("X-Content-Type-Options") ?: throw UnauthorizedException()
+        val frame = request.getHeader("X-Frame-Options") ?: throw UnauthorizedException()
         val transportSecurity = request.getHeader("Strict-Transport-Security") ?: throw UnauthorizedException()
+        val xssProtection = request.getHeader("X-XSS-Protection") ?: throw UnauthorizedException()
+
+        if (contentTypeOption != "nosniff") {
+            throw UnauthorizedException()
+        }
+
+        if (frame != "SAMEORIGIN") {
+            throw UnauthorizedException()
+        }
 
         if (transportSecurity != "max-age=31536000; includeSubDomains; preload") {
             throw UnauthorizedException()
         }
+
+        if (xssProtection != "1; mode=block") {
+            throw UnauthorizedException()
+        }
+
         // valid
     }
 
